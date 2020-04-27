f5f5fdaf2ff694bc83f9a721f3952e9d072931f1
iremmelci <48402907+iremmelci@users.noreply.github.com>
2020-04-27 14:00:55 +0300

    Add files via upload

---
+++ StackArrayImp.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+#define MAX 1000 // Macro definitions allows to declar constant values
+class Stack {
+	int top;
+public:
+	int a[MAX]; // Maximum size of Stack
+	Stack()
+	{
+		top = -1;
+	}
+	void push(int x);
+	int pop();
+	int peek();
+	bool isEmpty();
+};
+void Stack::push(int x)
+{
+	if (top >= (MAX - 1)) {
+		cout << "Stack Overflow" << endl;
+	}
+	else {
+		a[++top] = x;
+		cout << x << " pushed into stack\n" << endl;
+	}
+}
+int Stack::pop()
+{
+	if (top < 0) {
+		cout << "Stack Underflow" << endl;
+		return 0;
+	}
+	else {
+		int x = a[top--];
+		return x;
+	}
+}
+int Stack::peek()
+{
+	if (top < 0) {
+		cout << "Stack is Empty" << endl;
+		return 0;
+	}
+	else {
+		return a[top];
+	}
+}
+bool Stack::isEmpty()
+{
+	return (top < 0);
+}
+// Driver program to test above functions
+int main()
+{
+	class Stack s;
+	s.pop();
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	cout << s.peek() << endl;
+	cout << s.pop() << " Popped from stack\n";
+	getchar();
+}
